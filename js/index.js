ffc9c7c21e6f560182b85c9bcef31152da346247
Ing <phorntharith090@gmail.com>
2021-06-04 23:12:19 +0700

    - add type, duration, auto hide, progressbar - add gitignore

--- js/index.js
+++ js/index.js
@@ -12,14 +12,14 @@
         console.log('Isomorphic compatibility is not supported at this time for toast.')
     }
 })(this, function () {
-    
+
     // We need DOM to be ready
     if (document.readyState === 'complete') {
         init();
     } else {
         window.addEventListener('DOMContentLoaded', init);
     }
-    
+
     // Create toast object
     toast = {
         // In case toast creation is attempted before dom has finished loading!
@@ -31,14 +31,14 @@
         }
     };
     var autoincrement = 0;
-    
+
     // Initialize library
     function init() {
         // Toast container
         var container = document.createElement('div');
         container.id = 'toasting-container';
         document.body.appendChild(container);
-        
+
         // @Override
         // Replace create method when DOM has finished loading
         toast.create = function (options) {
@@ -51,41 +51,39 @@
             var toast = document.createElement('div');
             toast.id = ++autoincrement;
             toast.id = 'toast-' + toast.id;
-            toast.className = 'cooltoast-toast';
-
+            toast.className = 'toasting-toast';
 
             var wrapper = document.createElement('div');
-            
-            
+
             // title
             if (options.title) {
                 var h4 = document.createElement('h4');
-                h4.className = 'cooltoast-title';
+                h4.className = 'toasting-title';
                 h4.innerHTML = options.title;
                 wrapper.appendChild(h4);
             }
-            
+
             // text
             if (options.text) {
                 var p = document.createElement('p');
-                p.className = 'cooltoast-text';
+                p.className = 'toasting-text';
                 p.innerHTML = options.text;
                 wrapper.appendChild(p);
             }
-            
+
             // icon
             if (options.icon) {
                 var img = document.createElement('img');
                 img.src = options.icon;
-                img.className = 'cooltoast-icon';
+                img.className = 'toasting-icon';
                 wrapper.appendChild(img);
             }
 
             if (typeof options.hideProgressBar === 'boolean' && !options.hideProgressBar) {
 
-                var cssAnimation    = document.createElement('style');
-                cssAnimation.id     = `style-${toast.id}`
-                cssAnimation.type   = 'text/css';
+                var cssAnimation = document.createElement('style');
+                cssAnimation.id = `style-${toast.id}`
+                cssAnimation.type = 'text/css';
                 var rules = document.createTextNode(`
                     @keyframes animate-${autoincrement} {
                         0% {
@@ -99,40 +97,47 @@
                 cssAnimation.appendChild(rules);
                 document.getElementsByTagName('head')[0].appendChild(cssAnimation);
 
-                var progressBar                             = document.createElement('div');
-                progressBar.classList                       = 'progress-bar';
-                progressBar.style.animationName             = `animate-${autoincrement}`;
-                progressBar.style.animationDuration         = `${timeout/1000}s`;
-                progressBar.style.animationTimingFunction   = 'linear';
-                progressBar.style.animationFillMode         = 'forwards';
+                var progressBar = document.createElement('div');
+                progressBar.classList = 'progress-bar';
+                progressBar.style.animationName = `animate-${autoincrement}`;
+                progressBar.style.animationDuration = `${timeout / 1000}s`;
+                progressBar.style.animationTimingFunction = 'linear';
+                progressBar.style.animationFillMode = 'forwards';
+
+                if (typeof options.progressBarType !== 'undefined' && options.progressBarType === 'rainbow') {
+                    console.log(true);
+                    progressBar.classList += ' rainbow';   
+                } else {
+                    console.log(false);
+                }
 
                 wrapper.appendChild(progressBar);
             }
-            
+
             // click callback
             if (typeof options.callback === 'function') {
                 toast.addEventListener('click', options.callback);
             }
-            
+
             // toast api
             toast.hide = function () {
-                toast.className += ' cooltoast-fadeOut';
+                toast.className += ' toasting-fadeOut';
                 toast.addEventListener('animationend', removeToast, false);
 
             };
-            if(typeof options.autoHide === 'undefined') {
+            if (typeof options.autoHide === 'undefined') {
                 setTimeout(toast.hide, timeout + 200);
             } else if (typeof options.autoHide === 'boolean' && options.autoHide) {
                 setTimeout(toast.hide, timeout + 200);
             }
-            
+
             if (options.type) {
-                toast.className += ' cooltoast-' + options.type;
+                wrapper.className += ' toasting-' + options.type;
             }
-            
+
             toast.addEventListener('click', toast.hide);
-            
-            
+
+
             function removeToast() {
                 document.getElementById('toasting-container').removeChild(toast);
                 let style = document.querySelector(`#style-${toast.id}`);
@@ -142,36 +147,49 @@
             }
 
             toast.appendChild(wrapper);
-            
+
             document.getElementById('toasting-container').appendChild(toast);
             return toast;
-            
+
         }
     }
-    
+
     return toast;
-    
+
 });
 
 let 
-    title   = '',
-    text    = '';
+    title           = '',
+    text            = '',
+    type            = 'default',
+    progressBarType = 'rainbow',
+    autoHide        = false,
+    hideProgressBar = false,
+    duration        = 0;
 
-
+let a;
 function show() {
-    toast.create({
-        title           : title === '' ? 'Default Title': title,
+    a = toast.create({
+        title           : title === '' ? 'Default Title' : title,
         text            : text,
-        hideProgressBar : false,
-        timeout         : 4000,
-        autoHide        : false,
+        type            : type,
+        hideProgressBar : hideProgressBar,
+        progressBarType : progressBarType,
+        timeout         : isNaN(duration) || duration < 0 ? 0 : duration,
+        autoHide        : autoHide,
     });
 };
 
-    let btn = document.querySelector('#btn');
+let btn = document.querySelector('#btn');
 
-    btn.addEventListener('click', () => {
-        title   = document.querySelector('#title').value,
-        text    = document.querySelector('#text').value;
-        show(title, text);
-    });
+btn.addEventListener('click', () => {
+    title           = document.querySelector('#title').value;
+    text            = document.querySelector('#text').value;
+    type            = document.querySelector('input[name="type"]:checked').value;
+    progressBarType = document.querySelector('input[name="pType"]:checked').value;
+    autoHide        = document.querySelector('#autoHide').checked;
+    hideProgressBar = document.querySelector('#hideProgressBar').checked;
+    duration        = parseInt(document.querySelector('#duration').value);
+    // console.info({title, text, type, autoHide, hideProgressBar, duration});
+    show(title, text);
+});
